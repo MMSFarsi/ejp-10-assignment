0670b3a6fb7acf4c97b76725edf0c8f9dea5c5e3
mmsfarsi <mirmdsalmanfarsi.1@gmail.com>
2025-01-24 04:51:31 +0600

    problem fixed

--- src/app/Profile/page.jsx
+++ src/app/Profile/page.jsx
@@ -1,24 +1,28 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+export default async function Profile() {
+ 
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  if (!(await isAuthenticated())) {
+    redirect("/api/auth/login");
+  }
 
-const Profile = async () => {
-  const { getUser } = getKindeServerSession();
   const user = await getUser();
 
   return (
-    <div className="container mx-auto mt-10 p-6   ">
-    
-          <h1 className="text-3xl font-bold text-blue-600 mb-4">Welcome, {user.family_name || 'User'}!</h1>
-          <div className="text-lg">
-            <p>
-              <h2>Full Name:</h2> {user.given_name} {user.family_name}
-            </p>
-            <p>
-              <h2>Email:</h2> {user.email}
-            </p>
-          </div>
+    <div className="container mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-md">
+      <h1 className="text-3xl font-bold text-blue-600 mb-4">
+        Welcome to your Profile, {user?.family_name || "User"}!
+      </h1>
+      <div className="text-lg">
+        <p>
+          <span className="font-bold">Full Name:</span> {user?.given_name} {user?.family_name}
+        </p>
+        <p>
+          <span className="font-bold">Email:</span> {user?.email}
+        </p>
+      </div>
     </div>
   );
-};
-
-export default Profile;
+}
